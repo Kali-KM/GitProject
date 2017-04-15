22bf8317db3b95dc92ca3726071c9080650090d7
Kali-KM <rudals223@gmail.com>
2017-04-16 00:43:56 +0900

    Update CharArrayToIntArray.cpp

--- c/CharArrayToIntArray.cpp
+++ c/CharArrayToIntArray.cpp
@@ -36,3 +36,21 @@ int main()
 			break;
 	}
 }
+
+/*
+char* StringToIntArray(char *data)
+{
+	char arr[0x100];
+	char *pn = arr;
+	char *ptr = strtok(data, "");
+	while(1)
+	{
+		*pn++ = strtol(ptr, 0, 16);
+		ptr = strtok(NUL, " ");
+		if(ptr== NULL)
+			break;
+	}
+	return arr	
+}
+
+*/
