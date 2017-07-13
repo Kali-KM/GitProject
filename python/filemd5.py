ccbf2baccab3e118fa6804aa4d1af8beab5accdd
Kali-KM <rudals223@gmail.com>
2017-07-13 15:52:07 +0900

    odify a filemd5.py

--- python/filemd5.py
+++ python/filemd5.py
@@ -10,25 +10,29 @@ def main():
 	if len(sys.argv) == 1:
 		print "Usage : filemd5.py [DirPath]"
 		sys.exit(0)
-	DirPath = sys.argv[1]
-	md5 = hashlib.md5()
-	
+	target_dir = sys.argv[1]
+	root_path = ''
+	file_list = []
 
-	for root, dirs, files in os.walk(DirPath):
-		rootpath = os.path.join(os.path.abspath(DirPath), root)
+	for root, dirs, files in os.walk(target_dir):
+		root_path = os.path.join(os.path.abspath(target_dir), root)
 		for file in files:
-			filepath = os.path.join(rootpath, file)
-			md5 = hashlib.md5()
-			with open(filepath, 'rb') as f:
-				while True:
-					data = f.read(8192)
-					if not data:
-						break
-					md5.update(data)
-			file_hash = md5.hexdigest().upper()
-			filepath = "\"" + filepath + "\""
-			cmd = "rename " +filepath+" "+file_hash
-			os.system(cmd)
+			filepath = os.path.join(root_path, file)
+			file_list.append(filepath)
+			
+	md5 = hashlib.md5()
+
+	for file_path in file_list:
+		with open(file_path, 'rb') as f:
+			while True:
+				data = f.read(8192)
+				if not data:
+					break
+				md5.update(data)
+		hash_path =  "\"" + root_path+ "\\" + str(md5.hexdigest().upper()) + "\""
+		filepath = "\"" + file_path + "\""
+		cmd = "move /y " + filepath + " " + hash_path
+		os.system(cmd)
 
 if __name__ == '__main__':
 	main()
